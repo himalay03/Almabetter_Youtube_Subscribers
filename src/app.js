d6be5b64e6210794d126e7cbf2ff872fb3c82c17
himalay03 <114144373+himalay03@users.noreply.github.com>
2023-05-13 11:41:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,62 @@
+const express = require("express");
+const app = express();
 
-const express = require('express');
-const app = express()
+// Schema
+const schema = require("./models/subscribers.js");
 
+// Parse incoming Json data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Your code goes here
+// Home Page
+app.get("/", (req, res) => {
+  res.send(
+    "Hello, this is the home page of Youtube Subscribers backend project"
+  );
+});
 
+// This route shows all subscribers list with details
+app.get("/subscribers", (req, res) => {
+  schema
+    .find()
+    .then((subscribers) => {
+      res.send(subscribers);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
+});
 
+// This route only shows name and subscribedChannel of individuals
+app.get("/subscribers/names", (req, res) => {
+  schema
+    .find()
+    .select({
+      name: 1,
+      subscribedChannel: 1,
+      _id: 0,
+    })
+    .then((subscriber) => {
+      res.send(subscriber);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
+});
 
+// This route shows the details of a particular person with the help of Id
+app.get("/subscribers/:_id", (req, res) => {
+  const subscriberId = req.params._id;
+  // Get the value of the id parameter from the request URL
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  schema
+    .findById(subscriberId)
+    .then((subsId) => {
+      res.json(subsId);
+    })
+    .catch((error) => {
+      res.status(404).send({ message: error.message }, error);
+    });
+});
 
 module.exports = app;
